7e14e8e7c690823f583105780da1bbd8d05f063b
Joshmedina <83549474+joshmedina1998@users.noreply.github.com>
2024-02-16 16:17:10 +0800

    Update Server.js

--- backend/Server.js
+++ backend/Server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 const routes = require("./routes/TaskRoute");
-const path = require("path");
-const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,6 +11,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors()); // Enable CORS
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
+
+// Log incoming requests
+app.use((req, res, next) => {
+  console.log(`Received a ${req.method} request to ${req.url}`);
+  next();
+});
 
 mongoose
   .connect(process.env.MONGO_URI, {
@@ -21,5 +27,11 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use("/api", routes);
+
+// Basic error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
+});
 
 app.listen(PORT, () => console.log(`Listening at ${PORT}`));
